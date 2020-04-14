06a6eb2cbb1488cbf2b944e04e4927fedde336ef
Kevin Labtani <kevinlabtani@gmail.com>
2020-04-14 13:26:26 +0200

    react-hook-form register, handlesubmit and basic validation

--- prototyping/react-hook-form-basics/src/App.js
+++ prototyping/react-hook-form-basics/src/App.js
@@ -1,36 +1,38 @@
 import React from "react";
-import useForm from "react-hook-form";
+import { useForm } from "react-hook-form";
 import "./styles.css";
 
 function App() {
   const onSubmit = (data) => {
-    alert(JSON.stringify(data));
+    alert(JSON.stringify(data, null, 2));
   };
 
+  const { register, handleSubmit } = useForm();
+
   return (
-    <form className="App" onSubmit={onSubmit}>
+    <form className="App" onSubmit={handleSubmit(onSubmit)}>
       <h1>Sign Up</h1>
       <label>First Name:</label>
-      <input name="firstName" />
+      <input name="firstName" ref={register({ required: true })} />
 
       <label>Last Name:</label>
-      <input name="firstName" />
+      <input name="lastName" ref={register({ required: true })} />
 
       <label>Gender</label>
-      <select name="gender">
-        <option>Select...</option>
-        <option>Male</option>
-        <option>Female</option>
+      <select name="gender" ref={register({ required: true })}>
+        <option value="">Select...</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
       </select>
 
       <label>Username</label>
-      <input name="username" />
+      <input name="username" ref={register({ required: true })} />
 
       <label>Email</label>
-      <input name="email" />
+      <input name="email" ref={register({ required: true })} />
 
       <label>About you</label>
-      <textarea name="about you" />
+      <textarea name="about you" ref={register({ required: true })} />
 
       <input type="submit" />
     </form>
