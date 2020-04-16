291f2a920c0951619d2c9b768a7e64ee22a3587a
Kevin Labtani <kevinlabtani@gmail.com>
2020-04-16 14:21:16 +0200

    wizard form with formik + MUI - end

---
+++ prototyping/react-formik-wizard-form/src/components/Confirm.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import { List, ListItem, ListItemText } from "@material-ui/core/";
+import Header from "./Header";
+
+const useStyles = makeStyles((theme) => ({
+  textCenter: {
+    textAlign: "center",
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function Confirm({ formData, prevStep, nextStep }) {
+  const classes = useStyles();
+  const { firstName, lastName, email, occupation, city, bio } = formData;
+  return (
+    <>
+      <Header title="Confirm User Data" />
+      <div>
+        <List>
+          <ListItem>
+            <ListItemText
+              primary="First Name"
+              secondary={firstName}
+              className={classes.textCenter}
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Last Name"
+              secondary={lastName}
+              className={classes.textCenter}
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Email"
+              secondary={email}
+              className={classes.textCenter}
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Occupation"
+              secondary={occupation}
+              className={classes.textCenter}
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="City"
+              secondary={city}
+              className={classes.textCenter}
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Bio"
+              secondary={bio}
+              className={classes.textCenter}
+            />
+          </ListItem>
+        </List>
+        <div className={classes.textCenter}>
+          <Button
+            color="secondary"
+            variant="contained"
+            className={classes.button}
+            onClick={() => prevStep()}
+          >
+            Back
+          </Button>
+
+          <Button
+            color="primary"
+            variant="contained"
+            className={classes.button}
+            onClick={() => nextStep()}
+          >
+            Confirm & Continue
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ prototyping/react-formik-wizard-form/src/components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    margin: "auto",
+  },
+}));
+
+export default function Header({ title }) {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" className={classes.title}>
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
