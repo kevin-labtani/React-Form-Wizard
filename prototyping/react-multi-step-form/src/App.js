04e2637b5527b033d800da2250c18f6e0d3b6e3b
Kevin Labtani <kevinlabtani@gmail.com>
2020-05-25 15:41:13 +0200

    remove navbar and justify content to center of viewport

--- prototyping/react-multi-step-form/src/App.js
+++ prototyping/react-multi-step-form/src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AlertState from "./context/alert/AlertState";
 import Form from "./components/Form";
-import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
 const App = () => {
   return (
     <AlertState>
-      <div className="min-vh-100 d-flex flex-column justify-content-between bg-hu-grey">
-        <Navbar />
+      <div className="min-vh-100 d-flex flex-column justify-content-center bg-hu-grey">
         <div className="container">
           <Router>
             <Form />
