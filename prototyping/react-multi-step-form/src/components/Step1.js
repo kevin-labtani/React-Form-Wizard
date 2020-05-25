b788569e57ee60753a320765074d415f7c7b61a7
Kevin Labtani <kevinlabtani@gmail.com>
2020-05-25 14:31:57 +0200

    add avater and speech-bubble foranswer

--- prototyping/react-multi-step-form/src/components/Step1.js
+++ prototyping/react-multi-step-form/src/components/Step1.js
@@ -7,7 +7,7 @@ import isEmail from "validator/lib/isEmail";
 const Step1 = ({ values, inputChange }) => {
   const { setAlert } = useContext(AlertContext);
 
-  const { push } = useHistory();
+  const { push, goBack } = useHistory();
 
   const cont = (e) => {
     e.preventDefault();
@@ -16,6 +16,11 @@ const Step1 = ({ values, inputChange }) => {
     } else {
       values.email === "admin@demo.com" ? push("/step3") : push("/step2");
     }
+  };
+
+  const back = (e) => {
+    e.preventDefault();
+    goBack();
   };
 
   return (
@@ -33,8 +38,9 @@ const Step1 = ({ values, inputChange }) => {
           <h3>What's your email?</h3>
         </div>
       </div>
+
       <div className="row">
-        <div className="col-10 offset-1 col-lg-8 offset-lg-2 rounded-lg px-lg-5 py-4 my-2 shadow bg-hu-grey-1">
+        <div className="col-8 offset-1 col-lg-7 offset-lg-2 rounded-lg px-lg-5 py-4 my-2 shadow bg-hu-grey-1 speech-bubble-answer">
           <Alerts />
           <div className="form-group">
             <label htmlFor="email">
@@ -51,13 +57,30 @@ const Step1 = ({ values, inputChange }) => {
               placeholder="Enter your email here"
             />
           </div>
+        </div>
 
-          <br />
-
-          <div className="text-right">
-            <button className="btn btn-primary rounded-circle" onClick={cont}>
-              <i className="fas fa-arrow-down" />
-            </button>
+        <div className="col-2 col-lg-1 text-center align-self-center">
+          {/* <img
+            src="//placehold.it/300"
+            alt=""
+            className="rounded-circle img-fluid"
+          /> */}
+          <i className="fas fa-user-circle avatar" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-8 offset-lg-2 my-3">
+          <div className="row">
+            <div className="col-6">
+              <button className="btn btn-danger rounded-circle" onClick={back}>
+                <i className="fas fa-arrow-up" />
+              </button>
+            </div>
+            <div className="col-6 text-right">
+              <button className="btn btn-primary rounded-circle" onClick={cont}>
+                <i className="fas fa-arrow-down" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
