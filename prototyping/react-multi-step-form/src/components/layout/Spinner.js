5adb7c7a650764ef5f3c828523fffeacbbd8536a
Kevin Labtani <kevinlabtani@gmail.com>
2020-05-28 11:49:55 +0200

    use bootstrap loading widget instead of a gif

--- prototyping/react-multi-step-form/src/components/layout/Spinner.js
+++ prototyping/react-multi-step-form/src/components/layout/Spinner.js
@@ -1,12 +1,11 @@
 import React, { Fragment } from "react";
-import spinner from "./spinner.gif";
 
 export default () => (
   <Fragment>
-    <img
-      src={spinner}
-      style={{ width: "200px", margin: "auto", display: "block" }}
-      alt="Loading..."
-    />
+    <div className="row">
+      <div class="spinner-border text-primary m-auto" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
   </Fragment>
 );
