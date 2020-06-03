ae9116c64deeca225f96c657d983140133c0e9bf
Kevin Labtani <kevinlabtani@gmail.com>
2020-06-03 13:06:11 +0200

    add height to progressbar

--- prototyping/react-multi-step-form/src/components/layout/Footer.js
+++ prototyping/react-multi-step-form/src/components/layout/Footer.js
@@ -3,25 +3,29 @@ import { useLocation } from "react-router-dom";
 import { useQuestions } from "../../context/questions/QuestionsState";
 
 const Footer = () => {
-  let location = useLocation();
-  let currentLoc = location.pathname.slice(1);
+  const location = useLocation();
+  const currentLoc = location.pathname.slice(1);
 
   const [questionsState] = useQuestions();
-  const { questions } = questionsState;
-  let progress = ((currentLoc - 1) / questions.length) * 100;
+  const { questions, loading } = questionsState;
+
+  const progress = ((currentLoc - 1) / questions.length) * 100;
+
+  if (loading || !Number.isInteger(parseInt(currentLoc))) {
+    return null;
+  }
 
   return (
     <footer className="footer bg-hu-grey">
       <div className="container">
         <div className="row">
           <div className="col-5 offset-7 col-lg-4 offset-lg-8 mt-3 mb-3">
-            <div className="progress">
+            <div className="progress" style={{ height: "16px" }}>
               <div
                 className="progress-bar"
                 role="progressbar"
                 style={{ width: `${progress}%` }}
-              >
-              </div>
+              ></div>
             </div>
           </div>
         </div>
