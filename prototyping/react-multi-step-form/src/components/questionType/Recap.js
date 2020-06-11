af8caea4a71ce513fac07039571ad8e323cbb54f
Kevin Labtani <kevinlabtani@gmail.com>
2020-06-11 11:33:52 +0200

    recap question type working

--- prototyping/react-multi-step-form/src/components/questionType/Recap.js
+++ prototyping/react-multi-step-form/src/components/questionType/Recap.js
@@ -19,13 +19,30 @@ const Recap = ({ data, values, questions }) => {
   for (let [key, value] of Object.entries(values)) {
     let answer = value;
     let question = questions.filter((q) => q.question_id === parseInt(key));
-    if (question[0].question_type_id !== 1 && question[0].box_values) {
-      let choice = question[0].box_values.filter((val) => val.id === value);
-      answer = choice[0].label;
+    if (question[0].box_values) {
+      if (question[0].question_type_id !== 1) {
+        let choice = question[0].box_values.filter((val) => val.id === value);
+        if (choice[0]) {
+          answer = choice[0].label;
+        }
+      }
+      if (question[0].question_type_id === 1) {
+        answer = "";
+        value.forEach((element) => {
+          let choice = question[0].box_values.filter(
+            (val) => val.id === element
+          );
+          if (choice[0]) {
+            answer += choice[0].label + ", ";
+          }
+        });
+        answer = answer.substring(0, answer.lastIndexOf(","));
+      }
     }
     list.push(
       <li key={key} className="list-group-item">
-        {question[0].question_name} : {answer}
+        <p className="font-weight-bold my-1">{question[0].question_name}</p>
+        <p className="my-1">{answer}</p>
       </li>
     );
   }
@@ -34,7 +51,7 @@ const Recap = ({ data, values, questions }) => {
     <div className="jumbotron bg-hu-grey-1 text-center">
       <h1 className="mb-3">{questionTitle}</h1>
       <p className="lead">{questionSubtitle}</p>
-      <ul className="list-group">{list}</ul>
+      <ul className="list-group col-lg-10 mx-auto">{list}</ul>
       <button className="btn btn-primary btn-lg px-5 mt-3" onClick={fwd}>
         Submit
       </button>
