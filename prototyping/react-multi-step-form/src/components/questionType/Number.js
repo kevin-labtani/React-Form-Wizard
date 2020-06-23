b2f1bbb234f2362e578fbf2bc119100b0b3031f9
Kevin Labtani <kevinlabtani@gmail.com>
2020-06-23 16:48:40 +0200

    handle error when no params passed to Numbers question type

--- prototyping/react-multi-step-form/src/components/questionType/Number.js
+++ prototyping/react-multi-step-form/src/components/questionType/Number.js
@@ -24,7 +24,7 @@ const Number = ({ values, inputChange, data }) => {
   } = data;
 
   let min, max;
-  parameters.forEach((param) => {
+  parameters && parameters.forEach((param) => {
     if (param.name === "max_value") max = parseInt(param.value);
     if (param.name === "min_value") min = parseInt(param.value);
   });
