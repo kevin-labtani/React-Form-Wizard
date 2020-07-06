8499b512b9530d074f1ee52bd5f3afd1adbb3ea0
Kevin Labtani <kevinlabtani@gmail.com>
2020-07-06 12:08:25 +0200

    del old files

--- prototyping/react-multi-step-form/src/context/questions/questionsContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const questionsContext = createContext();
-
-export default questionsContext;
--- prototyping/react-multi-step-form/src/context/types.js
+++
@@ -1,4 +0,0 @@
-export const SET_ALERT = "SET_ALERT";
-export const REMOVE_ALERT = "REMOVE_ALERT";
-export const GET_QUESTIONS = "GET_QUESTIONS";
-export const QUESTION_ERROR = "QUESTION_ERROR";
